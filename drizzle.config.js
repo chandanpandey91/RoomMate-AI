05071d10156410d5d567d3af7ea8d0eb1a1a8506
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-12-21 05:31:08 +0530

    database connection added

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
+  schema: './config/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
     url: 'postgresql://neondb_owner:i4aQ2cpvFVxw@ep-green-dew-a53kxwzr.us-east-2.aws.neon.tech/roommate-ai?sslmode=require',
